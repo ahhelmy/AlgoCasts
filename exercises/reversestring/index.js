ad37dbd95ef4b4f5ba4b742aff0a566310dc7718
Ahmed Helmy <ahmed.helmy.1@gmail.com>
2020-01-03 23:04:44 +0200

    handled tutorials

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+//debugger;
+    return str.split('').reduce((rev, char)=> char + rev, '');
+}
+//reverse('lama')
 module.exports = reverse;
+// sol.1
+// function reverse(str) {
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+// sol.2
+// function reverse(str) {
+//     let reversed = '';
+//     for(let character of str){
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+// sol.3
+// function reverse(str) {
+//         return str.split('').reduce((rev, char)=> char + rev, '');
+// }
