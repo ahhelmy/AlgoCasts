42e37a6f5950f26a61c0f716d7db70c0fa6aa1e3
Ahmed helmy <ahmed.helmy.1@gmail.com>
2020-01-04 20:39:33 +0200

    fromLast Exercise

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -7,10 +7,25 @@
 //    const list = new List();
 //    list.insertLast('a');
 //    list.insertLast('b');
-//    list.insertLast('c');
+//    list.insertLast('c'); 
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (n > 0) {
+        fast = fast.next;
+        n--;
+    }
+
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next
+    }
+
+    return slow;
+}
 
 module.exports = fromLast;
